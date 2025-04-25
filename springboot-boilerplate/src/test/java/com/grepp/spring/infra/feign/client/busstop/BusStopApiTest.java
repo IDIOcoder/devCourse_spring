640a3c500d4bde0e3ea8195fc7309f238a2cd9fd
azi-prac <azimemory@gmail.com>
2025-04-25 11:54:39 +0900

    lec: llm setting

--- springboot-boilerplate/src/test/java/com/grepp/spring/infra/feign/client/busstop/BusStopApiTest.java
+++ springboot-boilerplate/src/test/java/com/grepp/spring/infra/feign/client/busstop/BusStopApiTest.java
@@ -1,10 +1,6 @@
 package com.grepp.spring.infra.feign.client.busstop;
 
 
-import static org.assertj.core.api.Assertions.*;
-
-import com.grepp.spring.infra.feign.error.FeignCommonException;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,18 +13,5 @@ class BusStopApiTest {
     
     @Value("${bus-stop.apikey}")
     String apiKey;
-    
-    @Test
-    public void feignTest(){
-        System.out.println(busStopApi.getBusStop());
-    }
-    
-    @Test
-    public void getBusStop_throwEx_invalidApiKey(){
-       assertThatThrownBy(() -> busStopApi.getBusStop())
-           .isInstanceOf(FeignCommonException.class)
-           .satisfies(ex -> {
-               System.out.println(ex);
-           });
-    }
+
 }
