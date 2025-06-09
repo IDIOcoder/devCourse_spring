8cc8ed852425260f17e082a6527734e4f16f0003
azi-prac <azimemory@gmail.com>
2025-06-09 15:02:04 +0900

    lec: gateway day 02 5

--- grepp-gateway/src/main/kotlin/com/grepp/spring/infra/security/oauth2/OAuth2SuccessHandler.kt
+++ grepp-gateway/src/main/kotlin/com/grepp/spring/infra/security/oauth2/OAuth2SuccessHandler.kt
@@ -26,8 +26,8 @@ class OAuth2SuccessHandler(
         val user = authentication.principal as OAuth2User
         val userInfo = OAuth2UserInfo.create(path, user)
 
-        val tokenDto = authService.processTokenSignin(userInfo!!.name
-            , user.authorities.joinToString(","))
+        val roles = user.getAttribute<ArrayList<String>>("roles")?.first()
+        val tokenDto = authService.processTokenSignin(userInfo!!.name, roles ?: "ROLE_ANONYMOUS")
 
         userBlackListRepository.deleteById(userInfo.name)
         TokenResponseExecutor.response(response, tokenDto)
