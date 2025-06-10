0d5184b6be9737003b625a83ca0db86a60df9633
azi-prac <azimemory@gmail.com>
2025-06-10 14:08:30 +0900

    lec: mail day01 5

--- grepp-gateway/src/main/kotlin/com/grepp/spring/filter/AuthHeaderFilter.kt
+++ grepp-gateway/src/main/kotlin/com/grepp/spring/filter/AuthHeaderFilter.kt
@@ -10,33 +10,38 @@ import org.springframework.web.servlet.function.*
 
 fun handlerFilterFnc() : HandlerFilterFunction<ServerResponse, ServerResponse>{
     return HandlerFilterFunction { request, next ->
-        val memberId = request.headers().header("x-member-id")
-        if(memberId.isEmpty()) {
-            val userId = request.attributes()["x-member-id"].toString() ?: "ANONYMOUS"
-            val role = request.attributes()["x-member-role"].toString() ?: "ROLE_ANONYMOUS"
-
+        if(!request.headers().asHttpHeaders().containsKey("x-member-id")){
+            val userId = request.attributes()["x-member-id"] ?: "ANONYMOUS"
+            val role = request.attributes()["x-member-role"] ?: "ROLE_ANONYMOUS"
             val modified = ServerRequest.from(request)
-                .header("x-member-id", userId)
-                .header("x-member-role", role)
+                .header("x-member-id", userId.toString())
+                .header("x-member-role", role.toString())
                 .build()
-
             next.handle(modified)
+        }else{
+            next.handle(request)
         }
-        next.handle(ServerRequest.from(request).build())
     }
 }
 
 @Configuration
 class RouteConfiguration{
+    val userService = route("user-service")
+        .GET("/api/member/**", http())
+        .POST("/api/member/**", http())
+        .before(uri("http://localhost:8082"))
+        .build()
+
+    val mailService = route("mail-service")
+        .GET("/mail/**", http())
+        .POST("/mail/**", http())
+        .before(uri("http://localhost:8083"))
+        .build()
 
     @Bean
     fun headerExistsRoute(): RouterFunction<ServerResponse> {
-        return route("general-route")
-            .GET("/**", http())
-            .POST("/**", http())
+        return userService
+            .and(mailService)
             .filter(handlerFilterFnc())
-            .before(uri("http://localhost:8082"))
-            .before(uri("http://localhost:8083"))
-            .build()
     }
 }
